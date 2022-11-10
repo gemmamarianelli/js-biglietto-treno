3808676854e45666996266fb136393ce3afa30b2
Gemma Marianelli <115584815+gemmamarianelli@users.noreply.github.com>
2022-11-10 15:48:29 +0100

    secondo push

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ let eta = prompt("inserisci eta");
 let x = 0.21
 let z = x*km
 console.log(z);
+
+let a = 18
+if(eta<a){
+         let c = z+20/100
+          console.log(c);
+} else if(eta>65){
+    let b = z+40/100
+    console.log(b);
+}
